56cf1e96cd7682ca1b41db123c0c613adcedad0f
Petri Dragos-Alin <dragospetri@yahoo.com>
2022-04-04 19:26:35 +0300

    First commit

---
+++ Stack.cpp
@@ -0,0 +1,25 @@
+#include "Stack.h"
+
+Stack::Stack() {
+    //TODO IMPLEMENTATION
+};
+
+Stack::~Stack() {
+    //TODO IMPLEMENTATION
+};
+
+void Stack::Push(TElem e) {
+    //TODO IMPLEMENTATION
+};
+
+TElem Stack::Pop() {
+    //TODO IMPLEMENTATION
+};
+
+TElem Stack::Peek() {
+    //TODO IMPLEMENTATION
+};
+
+bool Stack::isEmpty() {
+    //TODO IMPLEMENTATION
+};
---
+++ Stack.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <tuple>
+using namespace std;
+
+//DO NOT CHANGE THIS PART
+typedef int TElem;
+#define NULL_TELEM (-11111)
+
+
+class Stack
+{
+
+private:
+    int cap;
+    int size;
+    int head;
+    int tail;
+    int next_empty;
+    std::tuple<int,int,TElem> *arr;
+
+
+public:
+    //constructor
+    Stack();
+
+    //destructor
+    ~Stack();
+
+    // Pushes Element on top of stack
+    void Push(TElem e);
+
+    // Pop (takes out) Element from the top of the stack
+    //throws exception if the stack is empty
+    TElem Pop();
+
+    // returns the value on top of the stack without taking it out
+    //throws exception if the stack is empty
+    TElem Peek();
+
+    // checks if the stack is empty
+    bool isEmpty();
+};
