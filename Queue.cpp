ddc4a0235ead064ee1799b7a704294fc26a0946d
Alex Piciorus <piciorusalexandru3@gmail.com>
2022-04-16 17:43:25 +0300

    modify

--- Queue.cpp
+++ Queue.cpp
@@ -13,7 +13,10 @@ Queue::Queue() {
     head=-1;
     arr = new tuple<int,TElem>[cap];
     for(int i=0;i<cap;i++)
-        get<0>(arr[i])=i;
+    {
+        get<0>(arr[i])=NULL_TELEM;
+        get<1>(arr[i])=NULL_TELEM;
+    }
     first_empty=0;
 }
 
@@ -48,18 +51,22 @@ void Queue::push(TElem elem) {
 
     {
 
-        get<0>(arr[first_empty])=-1;
+        int nod=head;
+        while(get<0>(arr[nod])!=-1)
+        {
+            nod=get<0>(arr[nod]);
+        }
+
+        get<0>(arr[nod])=first_empty;
         get<1>(arr[first_empty])=elem;
-        get<0>(arr[first_empty-1])=first_empty;
-        if(first_empty<head)
-        {
-            get<0>(arr[first_empty])=first_empty+1;
-            head=first_empty;
-        }
-        while((first_empty != head && get<0>(arr[first_empty]) == -1) || (get<0>(arr[first_empty])==head && first_empty<head))
-        {
-            first_empty=first_empty+1;
-        }
+        get<0>(arr[first_empty])=-1;
+
+        for(int i =0 ;i<cap;i++)
+            if(get<0>(arr[i])==NULL_TELEM)
+            {
+                first_empty=i;
+                break;
+            }
 
         size++;
     }
@@ -83,27 +90,22 @@ TElem Queue::pop() {
 
     TElem sters = top();
 
+
     if(size==1)
     {
         head=-1;
         first_empty=0;
         size--;
-    }
+    } else {
 
-    int nod=head;
-    first_empty=0;
-    if(nod != -1)
-    {
-        if(nod == head)
-        {
-            head=get<0>(arr[head]);
-        }
-        get<0>(arr[nod])=first_empty;
-        get<1>(arr[nod])=0;
-
-        first_empty=nod;
+        int old_pos = head;
+        first_empty = head;
+        head = get<0>(arr[head]);
+        get<0>(arr[old_pos]) = NULL_TELEM;
+        get<1>(arr[old_pos]) = NULL_TELEM;
         size--;
     }
+
 
 //
 
